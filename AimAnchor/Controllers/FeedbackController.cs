fa31182546fe0d7c91389e51b3c6aa2ef828fb83
Miran Qarachatani <miranariff1@gmail.com>
2023-11-20 12:32:40 -0500

    add feedback view and controller, feedback section on each goal to be added

---
+++ AimAnchor/Controllers/FeedbackController.cs
@@ -0,0 +1,37 @@
+﻿using AimAnchor.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AimAnchor.Controllers
+{
+    public class FeedbackController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        // constructor using db dependency
+        public FeedbackController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // fetch the goalsets and display them in index
+            var goalSets = _context.GoalSets.OrderBy(c => c.Title).ToList();
+            return View(goalSets);
+        }
+
+        //GET/Feedback/ByGaolSet/{title}
+        public IActionResult ByGoalSet(string title)
+        {
+            if (title == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewData["GoalSet"] = title;
+
+            var goals = _context.Goals.Where(g=> g.GoalSet.Title == title).OrderBy(g => g.Title).ToList();
+
+            return View(goals);
+        }
+    }
+}
